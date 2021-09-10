1bd1aa231d44c5da2971df5e7c92f2d0a096bec8
Ilya P <togowiththeflow@gmail.com>
2021-09-10 22:57:48 +0300

    feat: add error handlers

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.get('/whats-new', (req, res) => {
 	res.send('Whats up!');
 });
 
+app.all('*', (req, res, next) => {
+	// TODO: send 4xx page
+	res.send('I do not understand you');
+});
+
+app.use((err, req, res, next) => {
+	console.error(err);
+	// TODO: send 5xx page
+	res.send('I am sleeping');
+});
+
 app.disable('x-powered-by');
 app.disable('etag');
 
