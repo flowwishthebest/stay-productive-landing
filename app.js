190018a49d2d53488a7db4423327ec50a064a8a7
Ilya P <togowiththeflow@gmail.com>
2021-09-10 22:37:37 +0300

    feat: separate app & boot

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 'use strict';
 
 const express = require('express');
-const config = require('./config');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
-app.listen(config.port, () => {
-  console.log(`Example app listening at http://localhost:${config.port}`)
-});
+module.exports = app;
