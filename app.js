cdbbf97455e482cc159e5e980a82a8f7808321a4
Ilya P <togowiththeflow@gmail.com>
2021-09-10 22:34:02 +0300

    feat: add config

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 'use strict';
 
 const express = require('express');
+const config = require('./config');
 
 const app = express();
-
-const port = 3080;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(config.port, () => {
+  console.log(`Example app listening at http://localhost:${config.port}`)
 });
---
+++ config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+	port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3080,
+};
